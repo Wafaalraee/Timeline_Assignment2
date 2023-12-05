56536d6e9bbfd3e819a342b2953e4dc1a2a26378
wafa alraeai <wafaalraee9@gmail.com>
2023-12-05 18:53:16 +0100

    first commit

--- model/Article.js
+++ model/Article.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const post = new Schema({
-    title: {
+    name: {
         type: String,
         required: true,
         maxLength: [15, 'The title must be less than 15 letters.'],
     },
-    article: {
+    message: {
         type: String,
         required: true,
         maxLength: [40, 'The article must be less than 40 letters.'],
